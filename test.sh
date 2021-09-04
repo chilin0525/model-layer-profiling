93eae83002b0794dcc4f77a72c2054455b8e7108
chilin0525 <sky707070.cv07@nctu.edu.tw>
2021-09-04 22:47:45 +0800

    add SSD example script

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
-python3 ./src/main.py -c "python3 ./src/pytorch_profiler.py resnet50.pt 0" -t pytorch
-
 # pyprof: pyprof 產出的結果
 # pyprof_sample.py: 使用 pyprof 的範例
 # layer_inference_result: dlprof 的結果
+
+# resnet50
+python3 ./src/main.py -c "python3 ./src/pytorch_profiler.py resnet50.pt 0" -t pytorch
+
+# SSD
+python3 ./src/main.py -c "python3 ./src/ssd_profiler.py" -t pytorch
