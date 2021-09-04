984785e324e2d03dea9c6f3597809b655b841843
chilin0525 <sky707070.cv07@nctu.edu.tw>
2021-09-04 17:21:48 +0800

    add: remark

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
 python3 ./src/main.py -c "python3 ./src/pytorch_profiler.py resnet50.pt 0" -t pytorch
+
+# pyprof: pyprof 產出的結果
+# pyprof_sample.py: 使用 pyprof 的範例
+# layer_inference_result: dlprof 的結果
